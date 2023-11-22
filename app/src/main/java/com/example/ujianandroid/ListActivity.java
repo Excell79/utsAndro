d902166f37017ae95f0323398fea47f59e58898c
Excell Novale <excellsmrg@gmail.com>
2023-11-22 17:16:50 +0700

    UTS berubah lagi

--- app/src/main/java/com/example/ujianandroid/ListActivity.java
+++ app/src/main/java/com/example/ujianandroid/ListActivity.java
@@ -23,7 +23,7 @@ public class ListActivity extends AppCompatActivity {
             daftar_nama.add("data masih kosong");
         }
 
-
+        // Perulangan 1-20
         ArrayList<String> repeatedNames = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
             if ((i + 1) % 2 == 0) {
